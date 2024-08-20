5ceb90b855337db65015045ae663f6b410c73018
Monika Krawczuk <monikakrawczuk@gmail.com>
2024-08-21 00:10:40 +0200

    05-03 - Identyfikacja płci na podstawie imienia

---
+++ main.js
@@ -0,0 +1,37 @@
+const isMaleName = (names) => {
+  const lowerCaseNames = names.toLowerCase();
+
+  if (lowerCaseNames === "bonawentura" || !lowerCaseNames.endsWith("a")) {
+    return true;
+  } else {
+    lowerCaseNames.endsWith("a");
+    return false;
+  }
+};
+
+console.log(isMaleName("Bonawentura"));
+console.log(isMaleName("bonawentura"));
+console.log(isMaleName("Krzysiek"));
+console.log(isMaleName("jacek"));
+console.log(isMaleName("anna"));
+console.log(isMaleName("Sylwia"));
+
+/////////////
+
+const isMaleName1 = (names) => {
+  if (names === "bonawentura" || !names.endsWith("a")) {
+    return true;
+  } else {
+    namesames.endsWith("a");
+    return false;
+  }
+};
+
+console.log(isMaleName("Bonawentura"));
+console.log(isMaleName("bonawentura"));
+console.log(isMaleName("Krzysiek"));
+console.log(isMaleName("jacek"));
+console.log(isMaleName("anna"));
+console.log(isMaleName("Sylwia"));
+
+//////sprawdzić opcję co się dzieje gdy w linijce 22 jest Bonawentura w ten sposób napisana
